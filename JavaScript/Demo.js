9f8e924342d703d8d253c45859841a80d4b2ffe3
panneerselvam_49 <panneerselvamramesh5@gmail.com>
2025-03-28 09:32:02 +0530

    JS

---
+++ JavaScript/Demo.js
@@ -0,0 +1,90 @@
+console.log("Hello world!!");
+let $='10' //variable
+let my=2 //varible
+console.log($+my)
+"use strict";
+a=10;
+console.log(a);
+let name="John";
+let admin=name;
+console.log(admin);
+let var1=`Hello`;
+console.log(var1);
+console.log(12/0);
+console.log(NaN+2);
+let num = 2 //Integer
+console.log(num);
+let str = "String" //String
+console.log(str);
+let bnum =BigInt(123487238949234870924343);
+console.log(bnum);
+let bool=true;
+console.log(bool);
+let nul=null;
+console.log(nul);
+let und;
+console.log(und);
+let n1=5;
+// n1+=5;
+console.log(n1);
+console.log(typeof(n1));
+let ex=6;
+console.log(ex**2); //Exponential
+let ex1=5.5;
+console.log(Math.pow(ex1,2)); //Same as exponential
+console.log(Math.log10(ex1));
+//Heirachy of operation
+let he=(150+50)/2;
+console.log(he);
+let inc=3;
+inc++;
+console.log(inc);
+console.log(inc);
+let bg = 4 > 1;
+console.log(bg);
+let bg1=1>5;
+console.log(bg1);
+const cns=45; //Constant
+// cns+=3;
+console.log(cns);
+let v1=Number(3);
+console.log(v1);
+console.log(typeof(bg));
+let non=NaN;
+console.log(non);
+let lo=3;
+let lo1=5;
+console.log(lo&lo1);
+function myfun(){
+    alert("This is alert!");
+}
+// function myf(){
+//     alert(`your age ${number} your register num is`);
+// }
+function myf(){
+let age=prompt("Test",'');
+alert(`your age ${age} years old!!`);
+}
+function con(){
+let con=confirm("Are you there??",'');
+alert('I am ok ${con} is that var');
+}
+let cas="123";
+console.log(typeof(Number(cas)));
+let cs="0";
+console.log(typeof(Boolean(cs)));
+let cas1=123;
+console.log(typeof(String(cas1)));
+// function boole(){
+//     alert(Boolean(0));
+// }
+let g;
+console.log(typeof(g));
+let tc=123;
+let cont=Boolean(tc);
+console.log(cont);
+const cos=12;
+cos=10;
+console.log(cos);
+let ccs=1;
+console.log(ccs);
