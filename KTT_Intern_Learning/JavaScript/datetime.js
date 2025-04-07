d61459535048a0919a4d0c13177b4757f7e75186
panneerselvam49 <panneerselvamramesh5@gmail.com>
2025-04-07 18:40:35 +0530

    dateandtime

---
+++ KTT_Intern_Learning/JavaScript/datetime.js
@@ -0,0 +1,16 @@
+let dt=new Date();
+console.log(dt);
+
+let de=new Date(2004, 4, 9);
+console.log(de);
+
+
+let des=new Date('2004, 4, 9');
+console.log(des);
+
+let date=new Date();
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.getDay());
+console.log(date.getMilliseconds());
