82886b3169ed1e360a6475ff7575f30fa1596d1d
panneerselvam49 <panneerselvamramesh5@gmail.com>
2025-04-09 18:54:51 +0530

    JavaScript Asynchronous methods

--- KTT_Intern_Learning/JavaScript/advancedfunctions.js
+++ KTT_Intern_Learning/JavaScript/advancedfunctions.js
@@ -29,3 +29,84 @@ console.log(add(1,2,3,4,5));
 
 let ar=[1,2,3];
 console.log(Math.max(...ar));
+
+//closures
+/**/
+function name(fname, lname){
+    function twoname(){
+        return fname +':'+lname;
+    }
+    return twoname();
+}
+console.log(name('abc','def'));
+
+function count(){
+    let c=0;
+    function add(){
+        return c++;
+    }
+    return add;
+}
+let a=count();
+console.log(a());
+console.log(a());
+console.log(a());
+
+//error
+// let aa=1;
+// function gy(){
+//     console.log(aa);
+//     let aa=2;
+// }
+// gy();
+
+function ta(){
+    phase='good';
+    console.log(phase);
+    var phase;
+}
+console.log(typeof(ta));
+
+let hn={
+    name:'pannneer',
+    age:20,
+    sayname(){
+        return this.name+':'+this.age;
+    }
+}
+console.log(hn.sayname());
+
+let fn = function fun(name) {
+    if (name) {
+        console.log('name not given');
+        return name;
+    } else {
+        return fun('Oops Name not provided');
+    }
+}
+let dup=fn;
+fn=null;
+console.log(dup());
+
+function fc(a){
+    return a.length;
+}
+console.log(fc('pannneer'));
+
+let rt=new Function('a','b', 'return a+b');
+console.log(rt(10,10));
+
+//scheduling settimeout and setinterval
+setTimeout(() => {
+    console.log('I\'m async function');
+}, 3000);
+
+let tmer = setInterval(() => console.log(('tick')), 2000);
+
+setTimeout(() => { clearInterval(tmer); console.log(('stop')); }, 5000);
+
+// window.currentUser = {
+//     name: "John"
+//   };
+//   console.log(currentUser.name);
+//   console.log(window.currentUser.name);
